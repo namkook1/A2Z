2944a6d42ad06e3fbfae61e322979577399997c8
namkook1 <namkook1@naver.com>
2021-01-20 19:47:49 +0900

    2021-01-20 TestController 어노테이션 추가

--- src/main/java/com/a2z/bill/web/TestController.java
+++ src/main/java/com/a2z/bill/web/TestController.java
@@ -1,7 +1,9 @@
 package com.a2z.bill.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
